47a7526ab27bdf570961e10b8cbc193c0c4256e4
Wes Gabbard <wes.gabbard@blackbaud.com>
2020-07-22 14:01:31 -0500

    move api key to env var

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withCSS = require('@zeit/next-css')
 
+//using dir env to load env variables locally but keep out of source control, replace with your own
 module.exports = withCSS({
   env: {
-    BASE_URL: 'https://fundraising.qa.stjude.org',
+    BASE_URL: 'https://fundraising.qa.stjude.org', // LO instance
     ADMIN_USERNAME: process.env.API_USERNAME, // add your api admin username here
     ADMIN_PASSWORD: process.env.API_PASSWORD, // add you api admin password here
+    API_KEY: process.env.ENV_API_KEY //api key for LO instance
   },
   exportTrailingSlash: true,
   exportPathMap: function () {
